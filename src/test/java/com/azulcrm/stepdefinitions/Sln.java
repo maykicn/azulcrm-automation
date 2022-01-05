6f497a93ef06432c3f1765553d3262855e4abf05
bilalsr <bilalsari13@gmail.com>
2022-01-05 12:23:25 -0500

    bilal de israr ediyorum

--- src/test/java/com/azulcrm/stepdefinitions/Sln.java
+++ src/test/java/com/azulcrm/stepdefinitions/Sln.java
@@ -3,7 +3,9 @@ package com.azulcrm.stepdefinitions;
 public class Sln {
 
     public static void main(String[] args) {
-        System.out.println("selen");
-        System.out.println("bilal");
+        System.out.println("selen olmasin bence");
+        System.out.println("bilal demistim");
+        System.out.println("bilalde israr ediyorum");
+
     }
 }
