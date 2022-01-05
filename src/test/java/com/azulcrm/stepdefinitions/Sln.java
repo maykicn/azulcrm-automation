930bb5e5addc99a64ddb90cf4f4ba77355e0cc1f
selenucar <slnucartx@gmail.com>
2022-01-05 11:21:22 -0600

    baska

--- src/test/java/com/azulcrm/stepdefinitions/Sln.java
+++ src/test/java/com/azulcrm/stepdefinitions/Sln.java
@@ -4,6 +4,6 @@ public class Sln {
 
     public static void main(String[] args) {
         System.out.println("selen");
-        System.out.println("bilal");
+        System.out.println("baska bir sey");
     }
 }
